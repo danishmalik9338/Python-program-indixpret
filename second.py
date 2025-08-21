133f1226f218949552408ab0d5e1500801369df0
danishmalik9338 <danishmalik9338@gmail.com>
2025-08-21 11:30:38 +0530

    update testing

---
+++ second.py
@@ -0,0 +1,14 @@
+
+Hindi =   int(input("Please enter marks HINDI: "))
+English = int(input("Please enter marks ENGLISH: "))
+Math =    int(input("Please enter marks MATHS: "))
+Science = int(input("Please enter marks SCIENCE: "))
+
+print("Please enter Subject total Marks")
+print("HINDI:",    Hindi)
+print("ENGLISH:",  English)
+print("MATHS:",    Math)
+print("SCIENCE:",  Science)
+
+Total = Hindi + English + Math + Science   
+print("Total Marks:", Total)
