b090ffeaf77dbb64a5090226dec925aafe31d54a
danishmalik9338 <danishmalik9338@gmail.com>
2025-08-21 11:16:53 +0530

    testin file

---
+++ first.py
@@ -0,0 +1,13 @@
+
+first_name =input("please enter first name:")
+second_name =input("please enetr secnd name:")
+user_id =input("please enter user id:")
+email =input("please enter email:")
+age =input("please enter age:")
+contact =input("please enetr contact:")
+
+print("Name:",    first_name,second_name)
+print("Id:",      user_id)
+print("Email:",   email)
+print("Age:",     age)
+print("Contact:", contact)
